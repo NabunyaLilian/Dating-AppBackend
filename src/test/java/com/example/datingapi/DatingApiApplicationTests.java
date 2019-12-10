459569ce1670c501859dfe3aaeb15ef660ceef8c
nabunya lilian <liliannabunya2@gmail.com>
2019-12-10 12:12:03 +0300

    initial commit

---
+++ src/test/java/com/example/datingapi/DatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
